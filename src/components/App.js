b33e790e4813339bf3915b83a68f98561257e87f
Marcelo Daniliauskas <marcelo.daniliauskas@gmail.com>
2022-09-23 18:03:00 -0300

    Iniciando repositorio - primeira versao do botao de busca

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,12 @@
 import { Card } from 'primereact/card';
+import  Busca from './Busca'
 
 
 const App = () => {
   return (
-    <Card>Content</Card> 
+    <Card>
+      <Busca />
+    </Card> 
   )  
 }
 
